8851ae5ed21337e75336eafbb4fa3696f6da1fe4
keurcien <keurcien.luu@gmail.com>
2026-04-08 10:02:34 +0200

    Reduce padding in page container

--- web/src/components/layout/page-container.tsx
+++ web/src/components/layout/page-container.tsx
@@ -9,7 +9,7 @@ export function PageContainer({ children, className }: PageContainerProps) {
 	return (
 		<div
 			className={cn(
-				"mx-auto w-full max-w-5xl px-4 pb-28 @min-screen-md/layout:px-8 @min-screen-xl/layout:max-w-6xl",
+				"mx-auto w-full max-w-5xl px-4 pb-12 @min-screen-md/layout:px-8 @min-screen-xl/layout:max-w-6xl",
 				className,
 			)}
 		>
